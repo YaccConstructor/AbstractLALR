93abccc83b5a27f798dddb80cd1e3c57e9161e83
Mihail-M <Mihail-maryfich@yandex.ru>
2015-03-18 01:17:43 +0300

    fix style

--- AbstractStack/AbstractStack/Program.fs
+++ AbstractStack/AbstractStack/Program.fs
@@ -15,43 +15,36 @@ module AbsStack =
         member val top = (1,1) with get, set//fixed later
         member val bot = (1,1) with get, set//fixed later
       
-        member a.fold =  
+        member this.fold =  
             let setNodes = new HashSet<int>()
-            for v in a.g.Vertices do
-            setNodes.Add (snd v) |> ignore
+            for v in this.g.Vertices do
+                snd v |> setNodes.Add  |> ignore
 
             let setEdges = new HashSet<int*int>()
-            for e in a.g.Edges do
-                if (snd e.Source <> snd e.Target) then setEdges.Add( (snd e.Source), (snd e.Target)) |> ignore
+            for e in this.g.Edges do
+                if (snd e.Source <> snd e.Target) then 
+                   (snd e.Source, snd e.Target) |> setEdges.Add  |> ignore
 
             let newGr = new AdjacencyGraph<vertex, Edge<vertex>>()
             for v in setNodes do 
-                newGr.AddVertex((v, v)) |> ignore
+                (v, v) |> newGr.AddVertex |> ignore
 
-            for e in setEdges do 
-                newGr.AddEdge(Edge ( (fst e, fst e) , (snd e , snd e))  )|> ignore
+            for (x,y) in setEdges do 
+                Edge ((x, x), (y , y)) |> newGr.AddEdge  |> ignore
 
-            a.g <- newGr
+            this.g <- newGr
             //labeled
-            a.bot <- (fst a.bot, fst a.bot)
-            a.top <- (fst a.top, fst a.bot)
+            this.bot <- (fst this.bot, fst this.bot)
+            this.top <- (fst this.top, fst this.bot)
 
-        //unuon operator
+        //union operator
         static member (+) (a:AbsStack, b:AbsStack) = 
             let c = new AbsStack()
-            for v in a.g.Vertices do
-                c.g.AddVertex(v) |> ignore
+            a.g.Edges |> c.g.AddVerticesAndEdgeRange |> ignore
+            b.g.Edges |> c.g.AddVerticesAndEdgeRange |> ignore
 
-            for v in b.g.Vertices do
-                c.g.AddVertex(v) |> ignore
+            Edge(b.bot, a.top) |> c.g.AddEdge  |> ignore
 
-            for e in a.g.Edges do
-                c.g.AddEdge(e) |> ignore
-
-            for e in b.g.Vertices do
-                c.g.AddVertex(e) |> ignore
-
-            c.g.AddEdge (Edge((b.bot), (a.top))) |> ignore
             c.bot <- a.bot
             c.top <- b.top
             c

75b0d534549cb1e7fbbe81fbbab5aed62325ebd1
Mihail-M <Mihail-maryfich@yandex.ru>
2015-05-14 20:59:53 +0300

    Added test with if else and last reduce

--- AbstractLALR.Test/gen.cmd
+++ AbstractLALR.Test/gen.cmd
@@ -2,8 +2,10 @@ fsyacc.exe ParserAR.fsy -o ParserAR.fs --module ParserAR
 fsyacc.exe ParserLAR.fsy -o ParserLAR.fs --module ParserLAR
 fsyacc.exe ParserEvenCountBracket.fsy -o ParserEvenCountBracket.fs --module ParserEvenCountBracket  
 
-fsyacc.exe ParserEvenCountBracketMagicEOF.fsy -o ParserEvenCountBracketMagicEOF.fs --module ParserEvenCountBracketMagicEOF  
+fsyacc.exe ParserEvenCountBracketMagicEOF.fsy -o ParserEvenCountBracketMagicEOF.fs --module ParserEvenCountBracketMagicEOF -v 
 
 fsyacc.exe ParserCalc.fsy -o ParserCalc.fs --module ParserCalc
 fsyacc.exe ParserSimpleCalc.fsy -o ParserSimpleCalc.fs --module ParserSimpleCalc 
+fsyacc.exe ParserIfElse.fsy -o ParserIfElse.fs --module ParserIfElse 
+
 
